516a9e4dfc819fea1a917ae39dcbecb8afc87dcc
JrValerio <amarovsjr81@gmail.com>
2024-11-01 19:17:53 -0300

    chore: configura ESLint e Prettier para padronização de código

---
+++ eslint.config.js
@@ -0,0 +1,41 @@
+import js from '@eslint/js';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettier from 'eslint-config-prettier';
+import importHelpers from 'eslint-plugin-import-helpers';
+import prettierPlugin from 'eslint-plugin-prettier';
+import globals from 'globals';
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parser: tsParser,
+      globals: { ...globals.node, ...globals.browser },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      prettier: prettierPlugin,
+      'import-helpers': importHelpers,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...tsPlugin.configs.recommended.rules,
+      ...prettier.rules,
+      'prettier/prettier': 'error',
+      'no-unused-vars': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'import-helpers/order-imports': [
+        'warn',
+        {
+          newlinesBetween: 'always',
+          groups: ['/^node:*/', 'module', ['parent', 'sibling', 'index']],
+          alphabetize: { order: 'asc', ignoreCase: true },
+        },
+      ],
+    },
+  },
+];
